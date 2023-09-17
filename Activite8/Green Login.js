ec583a19352483c1deac746523ab6689d53f630d
akhouhwail2701 <102318140+akhouhwail2701@users.noreply.github.com>
2023-09-17 10:37:29 -0700

    Add files via upload

---
+++ Activite8/Green Login.js
@@ -0,0 +1,6 @@
+document.getElementById("txtuser").onfocus = function () {
+    document.getElementById("user").style.visibility = "visible";
+}
+document.getElementById("txtpass").onfocus = function () {
+    document.getElementById("pass").style.visibility = "visible";
+}
